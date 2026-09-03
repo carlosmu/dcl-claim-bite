8da67b30ded8130d5b5a51833ac15a382c65dd11
carlosmu <carlos.damian.munoz@gmail.com>
2026-09-02 23:51:40 -0300

    Add placeholder music

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { setupUi } from './ui'
 import { setupMineProximity } from './mining/mine-proximity'
+import { setupMusic } from './audio/music'
 
 export function main() {
+    setupMusic()
     setupMineProximity()
     setupUi()
 }
