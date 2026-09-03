fb77a8fbd07781d9efcf4f696a2f89daae824773
carlosmu <carlos.damian.munoz@gmail.com>
2026-09-02 21:41:05 -0300

    Placeholder assets

---
+++ assets/scene/entity-names.ts
@@ -0,0 +1,15 @@
+// Auto-generated entity names from the scene
+
+
+/**
+ * Object containing all entity names in the scene for autocomplete support.
+ */
+export enum EntityNames {
+  Bank = "Bank",
+  Beach_Rock = "Beach Rock",
+  Beryl = "Beryl",
+  Beryl_2 = "Beryl_2",
+  Goldmine = "Goldmine",
+  House_5_M_Blue = "House 5 M Blue",
+  House_7_M_Blue = "House 7 M Blue",
+} 
